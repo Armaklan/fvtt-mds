50d487baf23ec46955f1bbee0c4432ff799abbc3
ZUBER Lionel Armaklan <lionel.zuber@armaklan.org>
2025-06-23 13:35:45 +0200

    Refactor character sheet

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import "./styles/main.scss";
-import { PlayerCharacterSheet } from "./module/sheets/PlayerCharacterSheet";
+import {PlayerCharacterSheet} from "./module/sheets/PlayerCharacterSheet";
 
 Hooks.once("init", async () => {
-  console.log("Custom System | Initialisation");
+    console.log("Custom System | Initialisation");
 
-  // Register custom sheets
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("fvtt-mds", PlayerCharacterSheet, {
-    types: ["playerCharacter"],
-    makeDefault: true
-  });
+    // Register custom sheets
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("fvtt-mds", PlayerCharacterSheet, {
+        types: ["playerCharacter"],
+        makeDefault: true
+    });
 
-  // Register Handlebars partials
-  loadTemplates([
-    "systems/fvtt-mds/templates/dice/dice-roll-dialog.hbs",
-    "systems/fvtt-mds/templates/dice/dice-roll-result.hbs",
-  ]);
+    // Register Handlebars partials
+    loadTemplates([
+        "systems/fvtt-mds/templates/dice/dice-roll-dialog.hbs",
+        "systems/fvtt-mds/templates/dice/dice-roll-result.hbs",
+        "systems/fvtt-mds/templates/sheets/partials/aels.hbs",
+        "systems/fvtt-mds/templates/sheets/partials/blessure.hbs",
+        "systems/fvtt-mds/templates/sheets/partials/equipement.hbs",
+        "systems/fvtt-mds/templates/sheets/partials/technique.hbs",
+        "systems/fvtt-mds/templates/sheets/partials/header.hbs",
+        "systems/fvtt-mds/templates/sheets/partials/profile.hbs"
+    ]);
 });
